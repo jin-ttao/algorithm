39fe2023726a15dc33e7df1abe9b3ff61b765104
Jintae Song <133551021+jin-ttao@users.noreply.github.com>
2024-08-05 21:20:05 +0900

    Time: 57 ms (21.35%), Space: 49 MB (16.01%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,14 @@
+var createCounter = function(n) {
+    let num = n - 1;
+    return function() {
+        num++;
+        return num;
+        }
+    };
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
