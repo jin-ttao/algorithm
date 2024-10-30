be82450ebb33f77231a733e424b5df1f9da65f1b
Jintae Song <133551021+jin-ttao@users.noreply.github.com>
2024-10-30 10:35:12 +0900

    Time: 6 ms (95.83%), Space: 49.8 MB (98.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,7 +3,7 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+const isAnagram = function(s, t) {
     if (s.length !== t.length) {
         return false;
     }
@@ -15,11 +15,18 @@ var isAnagram = function(s, t) {
         diff[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
     }
     
-    for (let i = 0; i < diff.length; i++) {
-        if (diff[i] !== 0) {
+    for (let j = 0; j < diff.length; j++) {
+        if (diff[j] !== 0) {
             return false;
         }
     }
     
     return true;
 };
+
+/*
+input: 2개의 문자열 s, t
+output: t가 s의 anagram이면 true, 아니면 fasle 값 반환(Boolean)
+
+approach: anagram은 동일한 알파벳 문자를 갖고 있으므로, 특정 알파벳을 포함한 수를 뽑아서 순서와 관계 없이 철자만 확인한다.
+*/
