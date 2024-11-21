3eada628e81a40e43491a806d028971994363ea8
Jintae Song <133551021+jin-ttao@users.noreply.github.com>
2024-11-21 23:25:54 +0900

    Time: 0 ms (100.00%), Space: 51.9 MB (67.25%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function(root) {
+    if (root == null)  {
+        return 0;
+    }
+
+    let leftDepth = maxDepth(root.left);
+    let rightDepth = maxDepth(root.right);
+
+    return Math.max(leftDepth, rightDepth) + 1;
+};
+
+/*
+실험
+const node3 = new TreeNode(3);
+// 출력
+TreeNode {
+  val: 3,
+  left: undefined,
+  right: undefined,
+  __proto__: { constructor: ƒ TreeNode() }
+}
+
+*/
