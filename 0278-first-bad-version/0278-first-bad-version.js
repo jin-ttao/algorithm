0290b7ef37378b200f89e67c0ac63e86fe73b014
Jintae Song <133551021+jin-ttao@users.noreply.github.com>
2024-12-05 23:54:30 +0900

    Time: 50 ms (61.95%), Space: 49 MB (15.94%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+const solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let first = 1;
+        let last = n;
+
+        while (first < last) {
+            let mid = first + Math.floor((last - first) / 2);
+
+            if (isBadVersion(mid)) {
+                last = mid;
+            } else {
+                first = mid + 1;
+            }
+        }
+
+        return first;
+    };
+};
