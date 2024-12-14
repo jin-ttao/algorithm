f3e52b0593a4d4cf01193e01acb3d6e1bfa1143a
Jintae Song <133551021+jin-ttao@users.noreply.github.com>
2024-12-14 09:46:07 +0900

    Time: 0 ms (100.00%), Space: 49.3 MB (71.21%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -37,4 +37,8 @@ root.right = temp;
 console.log(root); // [4,7,2,6,9,1,3]
 ```
 - Tree 자료구조 참조형일까? invertTree(root.left); 로 변형이 일어난 결과가 root 원본에 계속 영향 줄지?
+    - 확인해본 내용: 트리는 참조형이라 원본이 계속 수정되고, 각 재귀 호출이 하위 트리 전체를 바꾸게 됨.
+    - 출처: We must realize that references will be maintained down the object tree, which is why we can accomplish this in O(n) time! (https://dev.to/nas5w/building-deep-trees-in-javascript-using-object-references-4565)
 */
+
+
