8103d42a1ec5696b52ac0b5f67588a59e4c6eeb6
Jintae Song <w.jinttao@gmail.com>
2025-03-09 23:02:58 +0900

    solve isAnagram

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,20 +7,20 @@ const isAnagram = function(s, t) {
     if (s.length !== t.length) {
         return false;
     }
-    
+
     let diff = new Array(26).fill(0);
 
     for (let i = 0; i < s.length; i++) {
         diff[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
         diff[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
     }
-    
+
     for (let j = 0; j < diff.length; j++) {
         if (diff[j] !== 0) {
             return false;
         }
     }
-    
+
     return true;
 };
 
