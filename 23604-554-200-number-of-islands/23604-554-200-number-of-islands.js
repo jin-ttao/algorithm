b9f91d22674bf59517f60539ab0657e4ff84bd56
Jintae Song <133551021+jin-ttao@users.noreply.github.com>
2025-03-25 00:28:32 +0900

    Time: 56 ms, Memory: 59.6 MB - LeetHub

--- 23604-554-200-number-of-islands/23604-554-200-number-of-islands.js
+++ 23604-554-200-number-of-islands/23604-554-200-number-of-islands.js
@@ -3,34 +3,35 @@
  * @return {number}
  */
 const numIslands = function(grid) {
-    const m = grid.length;
-    const n = grid[0].length;
-    let islands = 0;
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const visited = Array(rows).fill([]).map(() => Array(cols).fill(false));
+    let islandCount = 0;
 
-    const dfs = function (row, col) {
-        if (row < 0 || row >= m || col < 0 || col >= n) {
+    const dfs = function(row, col) {
+        if (
+            row < 0 || col < 0 || row >= rows || col >= cols ||
+            visited[row][col] || grid[row][col] === "0"
+        ) {
             return;
         }
-        if (grid[row][col] === "0" || grid[row][col] === "checked") {
-            return;
-        }
-        
-        grid[row][col] = "checked";
 
-        dfs(row + 1, col);
+        visited[row][col] = true;
+
         dfs(row - 1, col);
-        dfs(row, col + 1);
+        dfs(row + 1, col);
         dfs(row, col - 1);
+        dfs(row, col + 1);
     };
-    
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === "1") {
-                dfs(i, j);
-                islands += 1;
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === "1" && !visited[r][c]) {
+                dfs(r, c);
+                islandCount += 1;
             }
         }
     }
 
-    return islands;
+    return islandCount;
 };
