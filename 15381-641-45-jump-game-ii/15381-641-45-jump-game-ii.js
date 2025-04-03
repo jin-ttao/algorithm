2b54c79d38e64da4a1051158e18bba01386a7dfa
Jintae Song <133551021+jin-ttao@users.noreply.github.com>
2025-04-03 22:58:34 +0900

    Time: 1 ms, Memory: 54.8 MB - LeetHub

---
+++ 15381-641-45-jump-game-ii/15381-641-45-jump-game-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const jump = function(nums) {
+    let near = 0;
+    let far = 0;
+    let jumps = 0;
+
+    while (far < nums.length - 1) {
+        let farthest = 0;
+
+        for (let i = near; i <= far; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+        near = far + 1;
+        far = farthest;
+        jumps += 1;
+    }
+
+    return jumps;    
+};
